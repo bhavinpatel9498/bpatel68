50207db266db4dadb2866a65d75249e221fdcc37
bhavinpatel9498 <33076423+bhavinpatel9498@users.noreply.github.com>
2018-09-18 16:36:37 -0500

    562 week4 final submission

--- itmd-562/week-04/bpatel68_week04.js
+++ itmd-562/week-04/bpatel68_week04.js
@@ -1,3 +1,5 @@
+     /* Function implemented by Bhavin Patel - A20410380 */
+
     "use strict";
 
     function sumCongruentModulo(inputArray, divisor, remainder) 
@@ -7,7 +9,7 @@
         
         if (inputArray == null || inputArray == "" || !Array.isArray(inputArray))
         {
-          alert('Please provide valid inputArray.');
+          //alert('Please provide valid inputArray.');
           console.log('Please provide valid inputArray.');
           return;
         }
@@ -17,7 +19,7 @@
    
         if (divisor == null || divisor == ""  || divisor == 0 || isNaN(divisor))
         {
-          alert('Please provide valid non zero divisor.');
+          //alert('Please provide valid non zero divisor.');
           console.log('Please provide valid non zero divisor.');
           return;
         }
@@ -27,7 +29,7 @@
         if(String(remainder) == "" || remainder == null || Number(remainder) === "0" || 
           (isNaN(remainder) && !(Number(remainder) === 0) ) )
         {
-          alert('Please provider valid remainder value.');
+          //alert('Please provider valid remainder value.');
           console.log('Please provider valid remainder value.');
           return;
         }
@@ -39,8 +41,6 @@
 
         for (const arrVal of inputArray) 
         {
-          //alert(arrVal);
-
           if(arrVal % divisor ===  remainder)
           {
               sumOfElements = sumOfElements + arrVal;
@@ -52,4 +52,5 @@
           +' is :'+sumOfElements); */
         console.log('sumCongruentModulo ['+inputArray+'] for divisor '+divisor + ' and remainder '+remainder 
           +' is :'+sumOfElements);
+
     }
