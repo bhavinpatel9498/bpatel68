0b5b9a1b0c64ac2341ef16a2f64a00e20676e5ff
bhavinpatel9498 <33076423+bhavinpatel9498@users.noreply.github.com>
2018-09-28 01:20:23 -0500

    week-05 initial scripts

---
+++ itmo-544/Week-05/create-env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## This file is used to initialize launched AWS instance
+sudo apt-get update
+sudo apt-get install git -y
+sudo apt-get install python-pip -y
+sudo apt-get install ruby gem ruby-dev -y
+sudo gem install bundler jekyll
+sudo jekyll new bhavin-awesome-site
+cd bhavin-awesome-site
+sudo bundle exec jekyll serve
---
+++ itmo-544/Week-05/create.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## This file contains all AWS CLI commands used to create and launch the instance
+
+aws ec2 create-key-pair --key-name bhavin_itmo544_key>/home/vagrant/bhavin_itmo544_key.key
+
+sudo chmod 600 /home/vagrant/bhavin_itmo544_key.key
+
+aws ec2 create-security-group --description "This is a default group for ITMO544 by bhavin" --group-name "itmo544-default-group-bhavin"
+
+aws ec2 authorize-security-group-ingress --group-name itmo544-default-group-bhavin --protocol tcp --port 22 --cidr 0.0.0.0/0
+
+aws ec2 authorize-security-group-ingress --group-name itmo544-default-group-bhavin --protocol tcp --port 80 --cidr 0.0.0.0/0
+
+aws ec2 authorize-security-group-ingress --group-name itmo544-default-group-bhavin --protocol tcp --port 4000 --cidr 0.0.0.0/0
+
+#us-west-2
+aws ec2 run-instances --image-id ami-0bbe6b35405ecebdb --count 1 --instance-type t2.micro --key-name bhavin_itmo544_key --security-groups itmo544-default-group-bhavin --user-data "file:///vagrant/create-env.sh" 
+
+#us-east-2
+#aws ec2 run-instances --image-id ami-0f65671a86f061fcd --count 1 --instance-type t2.micro --key-name bhavin_itmo544_key --security-groups itmo544-default-group-bhavin --user-data "file:///vagrant/create-env.sh" 
+
