c9694fc38d4d58d030924e9f280d891b9048e664
bhavinpatel9498 <33076423+bhavinpatel9498@users.noreply.github.com>
2018-09-29 01:12:15 -0500

    week-05 final scripts

--- itmo-544/Week-05/create-env.sh
+++ itmo-544/Week-05/create-env.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 ## This file is used to initialize launched AWS instance
-sudo apt-get update
-sudo apt-get install git -y
-sudo apt-get install python-pip -y
-sudo apt-get install ruby gem ruby-dev -y
-sudo gem install bundler jekyll
-sudo jekyll new bhavin-awesome-site
+echo "========== Installing Updates ===========" >>bhavin_initilization_log.txt
+sudo apt-get update >>bhavin_initilization_log.txt
+
+echo "========== Installing GIT ===========" >>bhavin_initilization_log.txt
+sudo apt-get install git -y >>bhavin_initilization_log.txt
+
+echo "========== Installing python pip ===========" >>bhavin_initilization_log.txt
+sudo apt-get install python-pip -y >>bhavin_initilization_log.txt
+
+echo "========== Installing ruby ===========" >>bhavin_initilization_log.txt
+sudo apt-get install ruby gem ruby-dev -y >>bhavin_initilization_log.txt
+
+echo "========== Installing jekyll ===========" >>bhavin_initilization_log.txt
+sudo gem install bundler jekyll >>bhavin_initilization_log.txt
+
+echo "========== Creating a new site ===========" >>bhavin_initilization_log.txt
+sudo jekyll new bhavin-awesome-site >>bhavin_initilization_log.txt
+
 cd bhavin-awesome-site
-sudo bundle exec jekyll serve
-sudo apt-get install links
+
+echo "========== Running Jekyll Server ==========="
+sudo bundle exec jekyll serve --host 0.0.0.0
