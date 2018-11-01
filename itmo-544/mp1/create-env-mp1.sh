20d6b070d26a4d0bc4620a4d8265ac6e0e6e050a
bhavinpatel9498 <33076423+bhavinpatel9498@users.noreply.github.com>
2018-11-01 00:00:37 -0500

    mp1 files positional params

--- itmo-544/mp1/create-env-mp1.sh
+++ itmo-544/mp1/create-env-mp1.sh
@@ -1,11 +1,65 @@
 #!/bin/bash
 
-if [$# != 6]
+#_=''
+
+if [ $# -ne 6 ]
 then
 
-	echo "Provide all positional parameters"
+	echo "Provide only six positional parameters"
 	exit 1
 
 fi
+
+if [ -z $1 ]
+then
+	echo "Provide valid Image-id"
+	exit 1
+fi
+
+
+if [ -z $2 ]
+then
+	echo "Provide valid Key-name"
+	exit 1
+fi
+
+
+if [ -z $3 ]
+then
+	echo "Provide valid Security-Group"
+	exit 1
+fi
+
+
+if [ -z $4 ]
+then
+    echo "Provide Instance Count"
+	exit 1
+fi
+
+
+if [ -z $5 ]
+then
+	echo "Provide valid ELB-name"
+	exit 1
+fi
+
+
+if [ -z $6 ]
+then
+	echo "Provide valid S3-bucket-name"
+	exit 1
+fi
+
+
+#Create EC2 Instance
+
+#aws ec2 run-instances --image-id $1 --count $4 --instance-type t2.micro --key-name $2 --security-groups $3 --user-data "file:///vagrant/week04_bhavin_itmo544_setup.sh"
+
+aws ec2 run-instances --image-id $1 --count $4 --instance-type t2.micro --key-name $2 --security-groups $3
+
+
+#Fetch Instance Ids in a variable
+
 
 echo "End of Script"
