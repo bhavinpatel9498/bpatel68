d310484aa33683c770abf9205f2dabc2089b2b47
bhavinpatel9498 <33076423+bhavinpatel9498@users.noreply.github.com>
2018-11-01 22:31:53 -0500

    load balancer mp1

---
+++ itmo-544/mp1/create-env-mp1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## This file is used to initialize launched AWS instance
+echo "========== Installing Updates ==========="
+sudo apt-get update
+
+echo "========== Installing GIT ==========="
+sudo apt-get install git -y
+
+echo "=========== Installing Apache ==========="
+sudo apt-get install apache2 -y
--- itmo-544/mp1/create-mp1.sh
+++ itmo-544/mp1/create-mp1.sh
@@ -54,9 +54,9 @@ fi
 
 #Create EC2 Instance
 
-#aws ec2 run-instances --image-id $1 --count $4 --instance-type t2.micro --key-name $2 --security-groups $3 --user-data "file:///vagrant/week04_bhavin_itmo544_setup.sh"
+#InstanceIdList=`aws ec2 run-instances --image-id $1 --count $4 --instance-type t2.micro --key-name $2 --security-groups $3 --query 'Instances[*].InstanceId' --output text`
 
-InstanceIdList=`aws ec2 run-instances --image-id $1 --count $4 --instance-type t2.micro --key-name $2 --security-groups $3 --query 'Instances[*].InstanceId' --output text`
+InstanceIdList=`aws ec2 run-instances --image-id $1 --count $4 --instance-type t2.micro --key-name $2 --security-groups $3 --availability-zones us-west-2b --user-data "file://./create-env-init-mp1.sh" --query 'Instances[*].InstanceId' --output text` 
 
 echo "Created Instances $InstanceIdList"
 
@@ -71,12 +71,29 @@ aws ec2 create-tags --resources $InstanceIdList --tags Key="InstanceOwnerStudent
 #Wait command to check if instances are running
 
 aws ec2 wait instance-running --instance-ids $InstanceIdList
+echo "Instances are running. Waiting for System status ok and Instance status ok."
 
-echo "Instances are running. Creating Load balancer now."
+
+#Wait command to check if system status is ok
+
+aws ec2 wait system-status-ok --instance-ids $InstanceIdList
+echo "system status ok"
+
+#Wait command to check if instance status is ok
+
+aws ec2 wait instance-status-ok --instance-ids $InstanceIdList
+echo "Instance status ok"
+
+
+echo "Creating Load balancer now."
 
 #Create load balancer
 
-aws elb create-load-balancer --load-balancer-name $5 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones us-west-2a us-west-2b us-west-2c
+groupid=`aws ec2 describe-security-groups --group-names $3 --query "SecurityGroups[*].GroupId" --output text`
+
+#aws elb create-load-balancer --load-balancer-name $5 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones us-west-2b --security-groups $groupid
+
+loadBalUrl=`aws elb create-load-balancer --load-balancer-name $5 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --availability-zones us-west-2b --security-groups $groupid --query 'DNSName' --output text`
 
 #Creating tags for load balancer
 
@@ -84,6 +101,7 @@ aws elb add-tags --load-balancer-name $5 --tags "Key=InstanceOwnerStudent,Value=
 
 echo "Load balancer created. Registering instances now."
 
+#aws elb configure-health-check --load-balancer-name $5 --health-check Target=TCP:80,Interval=30,UnhealthyThreshold=10,HealthyThreshold=10,Timeout=5
 
 #Registering instances with load balancer
 
@@ -92,14 +110,7 @@ aws elb register-instances-with-load-balancer --load-balancer-name $5 --instance
 echo "Instances registered with load balancer."
 
 
-#Configure load balancer security groups
-
-aws elb apply-security-groups-to-load-balancer --load-balancer-name $5 --security-groups $3
-
-echo "Security groups attached with load balancer"
-
-
-#Apply stickiness policy
+#Create stickiness policy
 
 
 aws elb create-lb-cookie-stickiness-policy --load-balancer-name $5 --policy-name $5-cookie-policy --cookie-expiration-period 60
@@ -108,14 +119,20 @@ aws elb create-lb-cookie-stickiness-policy --load-balancer-name $5 --policy-name
 echo "stickiness policy created"
 
 
-#Load balancer on HTTP
-
-aws elb create-load-balancer-listeners --load-balancer-name $5 --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80"
-
+#Apply stickiness policy
 
 aws elb set-load-balancer-policies-of-listener --load-balancer-name $5 --load-balancer-port 80 --policy-names $5-cookie-policy
 
-echo "Listener configured and stickiness policy applied"
+echo "Stickiness policy applied load balancer."
 
+
+#Waiting for Success message from Load Balancer
+
+echo "Waiting for instance to come online."
+
+aws elb wait instance-in-service --load-balancer-name $5 --instances $InstanceIdList
+
+echo "Load Balancer is Up now. Please use below URL."
+echo $loadBalUrl
 
 echo "End of Script"
