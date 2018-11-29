871a2aa6541d2ee0ee999206ced5dcb11f676625
bhavinpatel9498 <33076423+bhavinpatel9498@users.noreply.github.com>
2018-11-29 08:27:06 -0600

    updating google translate api

--- itmo-544/mp2/create-env-mp2-standalone.sh
+++ itmo-544/mp2/create-env-mp2-standalone.sh
@@ -6,6 +6,8 @@ sudo git clone git@github.com:illinoistech-itm/bpatel68.git
 
 cd ./bpatel68/itmo-544/mp2/node-proj-translator-job
 
+export GOOGLE_APPLICATION_CREDENTIALS=/home/ubuntu/bpatel68/itmo-544/mp2/node-proj-translator-job/translate-proj.json
+
 npm install
 
 #sudo chown -R ubuntu:ubuntu /home/ubuntu
